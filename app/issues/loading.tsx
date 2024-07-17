cc21b5fce712e2e22d8c1f175a061418b12c7142
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-07-17 16:20:27 +0100

    Add Loading Skeleton

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
-import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import IssueActions from "./IssueActions";
+import { Button, Table } from '@radix-ui/themes'
+import Link from 'next/link'
+import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import IssueActions from './IssueActions'
 
-const IssuePage = async () => {
-  await new Promise((resolve)=>setTimeout(resolve,5000))
-  const issues = await prisma.issue.findMany();
+const LoadingIssuesPage = () => {
+  const issues = [1,2,3,4,5]
   return (
-    <div>
-      <IssueActions/>
-
-      <Table.Root variant="surface">
+    <>
+    <IssueActions/>
+    <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
@@ -26,25 +24,25 @@ const IssuePage = async () => {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell>
-                {issue.title}
+                <Skeleton/>
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
+                <Skeleton/>
                 </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
+              <Skeleton/>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
+              <Skeleton/>
               </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
-      </Table.Root>
-    </div>
-  );
-};
+      </Table.Root></>
+    
+  )
+}
 
-export default IssuePage;
+export default LoadingIssuesPage
