e2241750a201210878fcd70345aed7ee402409db
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-07-19 14:08:53 +0100

    Add Dailog Alert

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,9 @@
 import prisma from "@/prisma/client";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Grid, Flex } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
 
 interface Props {
   params: { id: string };
@@ -16,12 +17,15 @@ const IssueDetailPage = async ({ params }: Props) => {
   });
   if (!issueDetail) notFound();
   return (
-    <Grid columns={{initial:"1",md:"2"}} gap="5">
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
-        <IssueDetails issueDetail={issueDetail}/>
+        <IssueDetails issueDetail={issueDetail} />
       </Box>
-      <Box>
-        <EditIssueButton issueId={issueDetail.id}/>
+      <Box maxWidth="8rem">
+        <Flex direction="column" gap="4">
+          <EditIssueButton issueId={issueDetail.id} />
+          <DeleteIssueButton issueId={issueDetail.id} />
+        </Flex>
       </Box>
     </Grid>
   );
