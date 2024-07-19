91699f46a9e52f568e0274f5a82a5743dbaf2a11
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-07-19 14:08:53 +0100

    Add Dailog Alert

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import React from "react";
+
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure to delete this issue?
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
+
+export default DeleteIssueButton;
