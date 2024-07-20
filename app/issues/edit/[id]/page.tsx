c501547372475a0a5da2cb09e1a258bd1ab16621
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-07-20 12:07:35 +0100

    set up NextAuth

--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -4,10 +4,10 @@ import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "./loading";
 
-const IssueForm = dynamic(()=>import("../../_components/IssueFrom"),
-{ssr:false,
-loading : ()=> <IssueFormSkeleton/>
-})
+const IssueForm = dynamic(() => import("../../_components/IssueFrom"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 interface Props {
   params: { id: string };
@@ -17,8 +17,8 @@ const EditIssuePage = async ({ params }: Props) => {
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
-  if(!issue) notFound()
-  return <IssueForm issue={issue}/>;
+  if (!issue) notFound();
+  return <IssueForm issue={issue} />;
 };
 
 export default EditIssuePage;
