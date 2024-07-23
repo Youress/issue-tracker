eb8c390621cf279f89cbf0396d66dea9f58f5751
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-07-23 17:29:00 +0100

    show toast notification

--- app/issues/[id]/AssignSelect.tsx
+++ app/issues/[id]/AssignSelect.tsx
@@ -4,34 +4,53 @@ import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+const AssignSelect = ({ issue }: { issue: Issue }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
 
-const AssignSelect = ({issue}:{issue:Issue}) => {
-const {data:users, error, isLoading} = useQuery<User[]>({
-  queryKey:["users"],
-  queryFn: ()=> axios.get("/api/users").then(res => res.data),
-  staleTime: 60 * 1000,
-  retry: 3
-})
-if(isLoading) return  <Skeleton/>
-if(error) return null
+  const handleOnChange = async (userId: string) => {
+    try {
+      await axios.patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId || null,
+      });
+
+      toast.success("Changes saved successfully");
+    } catch (error) {
+      toast.error("Changes could not be saved");
+    }
+  };
 
   return (
-    <Select.Root
-    defaultValue={issue.assignedToUserId || ""}
-     onValueChange={(userId)=>{
-      axios.patch(`/api/issues/${issue.id}`,{assignedToUserId:userId || null})
-    }}>
-      <Select.Trigger placeholder="Assign" />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          {users?.map((user)=><Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
-          
-          
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={handleOnChange}
+      >
+        <Select.Trigger placeholder="Assign" />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
