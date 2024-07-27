13ed6b319e3a3e9af7bb2784596dfdf6aab0c73c
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-07-27 14:19:10 +0100

    refcator

---
+++ app/issues/list/IssueTable.tsx
@@ -0,0 +1,81 @@
+import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
+import { Table } from "@radix-ui/themes";
+import NextLink from "next/link";
+import { IssueStatusBadge, Link } from "@/app/components";
+import { Issue, Status } from "@prisma/client";
+
+interface Props {
+    searchParams: {
+      status: Status;
+      orderBy: keyof Issue;
+      sortOrder: "asc" | "desc";
+      page: string;
+    };
+    issues: Issue[]
+  }
+
+
+const IssueTable = ({searchParams, issues}:Props) => {
+   
+      const toggleOrder = () => {
+        return !searchParams.sortOrder || searchParams.sortOrder === "desc"
+          ? "asc"
+          : "desc";
+      };
+  return (
+    <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            {columns.map((col) => (
+              <Table.ColumnHeaderCell className={col.className} key={col.value}>
+                <NextLink
+                  href={{
+                    query: {
+                      ...searchParams,
+                      orderBy: col.value,
+                      sortOrder: toggleOrder(),
+                    },
+                  }}
+                >
+                  {col.label}
+                </NextLink>
+                {searchParams.orderBy === col.value &&
+                  (searchParams.sortOrder === "asc" ? (
+                    <ArrowUpIcon className="inline" />
+                  ) : (
+                    <ArrowDownIcon className="inline" />
+                  ))}
+              </Table.ColumnHeaderCell>
+            ))}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+  )
+}
+const columns: { label: string; value: keyof Issue; className?: string }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+  export const columnNames = columns.map(col => col.value)
+
+export default IssueTable
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,11 +1,8 @@
-import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Issue, Status } from "@prisma/client";
-import { Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
-import NextLink from "next/link";
-import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
 import Pagination from "@/app/components/Pagination";
+import IssueTable, { columnNames } from "./issueTable";
 
 interface Props {
   searchParams: {
@@ -16,23 +13,15 @@ interface Props {
   };
 }
 const IssuePage = async ({ searchParams }: Props) => {
-  const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: "Issue", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
-  ];
+  
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
-  const orderBy = columns.map((col) => col.value).includes(searchParams.orderBy)
+  const orderBy = columnNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: searchParams.sortOrder }
     : undefined;
-  const toggleOrder = () => {
-    return !searchParams.sortOrder || searchParams.sortOrder === "desc"
-      ? "asc"
-      : "desc";
-  };
+  
   const page = parseInt(searchParams.page) || 1;
   const pageSize = 10;
   const issues = await prisma.issue.findMany({
@@ -48,54 +37,8 @@ const IssuePage = async ({ searchParams }: Props) => {
     <div>
       <IssueActions />
 
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((col) => (
-              <Table.ColumnHeaderCell className={col.className} key={col.value}>
-                <NextLink
-                  href={{
-                    query: {
-                      ...searchParams,
-                      orderBy: col.value,
-                      sortOrder: toggleOrder(),
-                    },
-                  }}
-                >
-                  {col.label}
-                </NextLink>
-                {searchParams.orderBy === col.value &&
-                  (searchParams.sortOrder === "asc" ? (
-                    <ArrowUpIcon className="inline" />
-                  ) : (
-                    <ArrowDownIcon className="inline" />
-                  ))}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssueTable searchParams={searchParams} issues={issues}/>
       <div className="flex items-center justify-center mt-4">
-        {" "}
         <Pagination
           pageSize={pageSize}
           itemCount={issueCount}
